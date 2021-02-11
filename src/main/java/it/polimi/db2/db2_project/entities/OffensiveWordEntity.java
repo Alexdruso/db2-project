5af448686f1b3c04d47ef2d95e806dbe655b06fd
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-02-11 16:05:10 +0100

    feat: complete offensive word entity

--- src/main/java/it/polimi/db2/db2_project/entities/OffensiveWordEntity.java
+++ src/main/java/it/polimi/db2/db2_project/entities/OffensiveWordEntity.java
@@ -5,28 +5,14 @@ import java.util.Objects;
 
 @Entity
 @Table(name = "offensive_word", schema = "db2")
+@NamedQuery(name = "OffensiveWord.findAll", query = "SELECT w FROM OffensiveWordEntity w")
 public class OffensiveWordEntity {
-    private Long id;
+    @Id
+    @Column(name = "word", nullable = false)
     private String word;
 
-    @Id
-    @GeneratedValue
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    @Id
-    @Column(name = "word")
     public String getWord() {
         return word;
-    }
-
-    public void setWord(String word) {
-        this.word = word;
     }
 
     @Override
@@ -36,9 +22,7 @@ public class OffensiveWordEntity {
 
         OffensiveWordEntity that = (OffensiveWordEntity) o;
 
-        if (!Objects.equals(word, that.word)) return false;
-
-        return true;
+        return Objects.equals(word, that.word);
     }
 
     @Override
