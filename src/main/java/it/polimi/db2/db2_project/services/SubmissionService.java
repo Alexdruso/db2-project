921f86e8bbf8bff84ce86b2863403517f1c7b69f
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-02-16 22:34:12 +0100

    feat: start SubmissionService

--- src/main/java/it/polimi/db2/db2_project/services/SubmissionService.java
+++ src/main/java/it/polimi/db2/db2_project/services/SubmissionService.java
@@ -4,23 +4,39 @@ import it.polimi.db2.db2_project.entities.AnswerEntity;
 import it.polimi.db2.db2_project.entities.QuestionEntity;
 import it.polimi.db2.db2_project.entities.QuestionnaireEntity;
 
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
+@Stateless
 public class SubmissionService {
 
-    public Optional<QuestionnaireEntity> getCurrentQuestionnaire() {
-        //TODO Check time from the server
+    @PersistenceContext
+    private EntityManager em;
+
+    public Optional<QuestionnaireEntity> findCurrentQuestionnaire() {
+        return findQuestionnaireByDate(new Date());
         return null;
     }
 
-    public List<QuestionEntity> getMarketingQuestions(long questionnaireId) {
+    public Optional<QuestionnaireEntity> findQuestionnaireByDate(Date date) {
+        return Optional.ofNullable(
+                em.createNamedQuery("Questionnaire.findByDate", QuestionnaireEntity.class)
+                        .setParameter("date", date)
+                        .getSingleResult()
+        );
+    }
+
+    public List<QuestionEntity> findMarketingQuestions(long questionnaireId) {
         //TODO
         return null;
     }
 
-    public List<QuestionEntity> getStatisticalQuestions(long questionnaireId) {
+    public List<QuestionEntity> findStatisticalQuestions(long questionnaireId) {
         //TODO
         return null;
     }
@@ -32,8 +48,8 @@ public class SubmissionService {
      * @return
      */
     public boolean submitMarketingAnswers(long userId, Map<Long, String> answers) {
-       //TODO CHeck badwors
-       //TODO Chcek all answers present
+        //TODO CHeck badwors
+        //TODO Chcek all answers present
         //TODO CHeck if already presented
         return false;
     }
