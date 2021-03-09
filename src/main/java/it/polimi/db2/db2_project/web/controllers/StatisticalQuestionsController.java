ff34aea9b72a6ab57266bf48d474442236945a63
Alessandro <alessandro1.sanvito@mail.polimi.it>
2021-03-09 17:37:14 +0100

    feat: statistical questions page

---
+++ src/main/java/it/polimi/db2/db2_project/web/controllers/StatisticalQuestionsController.java
@@ -0,0 +1,57 @@
+package it.polimi.db2.db2_project.web.controllers;
+
+import it.polimi.db2.db2_project.entities.QuestionEntity;
+import it.polimi.db2.db2_project.entities.QuestionnaireEntity;
+import it.polimi.db2.db2_project.services.SubmissionService;
+import it.polimi.db2.db2_project.web.TemplatingServlet;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import javax.ejb.EJB;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.HashMap;
+
+@WebServlet(name = "StatisticalQuestionsController", value = "/StatisticalQuestionsController")
+public class StatisticalQuestionsController extends TemplatingServlet {
+
+    @EJB
+    SubmissionService submissionService;
+
+    public StatisticalQuestionsController() {
+        super("statistical-questions", TemplateMode.HTML, "WEB-INF/templates/", ".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String, Object> context = new HashMap<>();
+        submissionService.findCurrentQuestionnaire().ifPresentOrElse(
+                questionnaire -> {
+                    context.put(
+                            "statisticalQuestions",
+                            submissionService.findStatisticalQuestions(questionnaire.getId())
+                    );
+                    context.put(
+                            "available",
+                            true
+                    );
+                },
+                () -> {
+                    context.put(
+                            "available",
+                            false
+                    );
+                }
+        );
+        super.processTemplate(request, response, context);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("POST invoked");
+
+        String path = getServletContext().getContextPath() + "/congratulations";
+        response.sendRedirect(path);
+    }
+}
