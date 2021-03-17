9fa0af30d8c8431619e9877cecb170cdd65a19e8
Davide Merli <davide.merli@outlook.com>
2021-03-17 12:24:54 +0100

    fix: missing EJB annotation

--- src/main/java/it/polimi/db2/db2_project/services/AdminService.java
+++ src/main/java/it/polimi/db2/db2_project/services/AdminService.java
@@ -2,34 +2,49 @@ package it.polimi.db2.db2_project.services;
 
 import it.polimi.db2.db2_project.entities.*;
 
+import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.Date;
 import java.util.List;
 
+@Stateless
 public class AdminService {
 
     @PersistenceContext
     private EntityManager em;
 
+    public QuestionnaireEntity getQuestionnaire(long questionnaireId) {
+        return em.createNamedQuery("Questionnaire.findById", QuestionnaireEntity.class)
+                .setParameter("id", questionnaireId)
+                .getSingleResult();
+    }
+
     public QuestionnaireEntity createQuestionnaire(long creatorUserId, long productId, Date date) {
         QuestionnaireEntity newQ = new QuestionnaireEntity();
         newQ.setDate(date);
         UserEntity creatorUser = em.find(UserEntity.class, creatorUserId);
+
         if(creatorUser == null) {
             throw new IllegalArgumentException(String.format("User with ID = %d does not exist!", creatorUserId));
         }
+
         ProductEntity product = em.find(ProductEntity.class, productId);
+
         if(product == null) {
             throw new IllegalArgumentException(String.format("Product with ID = %d does not exist!", productId));
         }
+
         newQ.setProductEntity(product);
         Date today = new Date();
+
         if(!date.after(today)) {
             throw new IllegalArgumentException("Questionnaire date must be set in the future");
         }
+
         newQ.setDate(date);
         em.persist(newQ);
+
         return newQ;
     }
 
@@ -43,18 +58,24 @@ public class AdminService {
 
     public QuestionnaireEntity addMarketingQuestion(long questionnaireId, String questionText) {
         QuestionnaireEntity questionnaire = em.find(QuestionnaireEntity.class, questionnaireId);
+
         if(questionnaire == null) {
             throw new IllegalArgumentException(String.format("Questionnaire with ID = %d does not exist!", questionnaireId));
         }
+
         if(questionText.equals("")) {
             throw new IllegalArgumentException("Question can't be empty");
         }
+
         QuestionEntity newQuestion = new QuestionEntity();
         newQuestion.setText(questionText);
         newQuestion.setOptional(false);
-        questionnaire.getQuestions().add(newQuestion);
+
         newQuestion.getQuestionnaires().add(questionnaire);
+        questionnaire.getQuestions().add(newQuestion);
+
         em.persist(newQuestion);
+
         return questionnaire;
     }
 
