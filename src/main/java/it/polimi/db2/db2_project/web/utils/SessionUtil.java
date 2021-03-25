2e84c7de93820aa38ea61719cb4b586aa6d78411
Davide Merli <davide.merli@outlook.com>
2021-03-25 23:06:02 +0100

    feat: add more utils for session handling

--- src/main/java/it/polimi/db2/db2_project/web/utils/SessionUtil.java
+++ src/main/java/it/polimi/db2/db2_project/web/utils/SessionUtil.java
@@ -1,40 +1,61 @@
 package it.polimi.db2.db2_project.web.utils;
 
-import it.polimi.db2.db2_project.entities.AnswerEntity;
+import it.polimi.db2.db2_project.entities.QuestionnaireEntity;
 import it.polimi.db2.db2_project.entities.UserEntity;
+import it.polimi.db2.db2_project.services.AdminService;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.websocket.Session;
-import java.net.http.HttpRequest;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class SessionUtil {
+
     public static Optional<UserEntity> checkLogin(HttpServletRequest request) {
-        //check user logged in
+        // Retrieve user from the session
         HttpSession session = request.getSession();
         UserEntity user = (UserEntity) session.getAttribute("user");
-        if(session.isNew() || user == null) {
+
+        // Return empty optional if the login status is invalid
+        if (session.isNew() || user == null) {
             return Optional.empty();
         }
+
         return Optional.of(user);
     }
 
-    public static void putAnswers(HttpServletRequest request, Map<Long, String>  answers) {
+    public static void saveAnswersToSession(HttpServletRequest request, Map<Long, String> answers) {
         HttpSession session = request.getSession();
         session.setAttribute("marketingAnswers", answers);
     }
 
     @SuppressWarnings("unchecked")
-    public static Map<Long, String> getAnswers(HttpServletRequest request) {
+    public static Map<Long, String> getAnswersFromSession(HttpServletRequest request) {
         HttpSession session = request.getSession();
 
-        Map<Long, String>  answers;
-        answers = (Map<Long, String> ) session.getAttribute("marketingAnswers");
-        if(session.isNew() || answers == null) {
+        Map<Long, String> answers;
+        answers = (Map<Long, String>) session.getAttribute("marketingAnswers");
+
+        if (session.isNew() || answers == null) {
             return new HashMap<>();
         }
+
         return answers;
+    }
+
+    public static Optional<QuestionnaireEntity> getQuestionnaire(AdminService adminService, HttpServletRequest request,
+                                                                 String parameterName) {
+        Optional<String> questionnaireId = Optional.ofNullable(request.getParameter(parameterName));
+
+        if (questionnaireId.isPresent()) {
+            if (questionnaireId.get().matches("\\d+")) {
+                long id = Long.parseLong(questionnaireId.get());
+
+                return adminService.getQuestionnaire(id);
+            }
+        }
+
+        return Optional.empty();
     }
 }
