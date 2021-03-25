85c114eb3edd12b430c0a030df37e624091b2ec6
Davide Merli <davide.merli@outlook.com>
2021-03-25 23:07:42 +0100

    fix: add static for maybe future use

--- src/main/java/it/polimi/db2/db2_project/web/utils/ImageUtil.java
+++ src/main/java/it/polimi/db2/db2_project/web/utils/ImageUtil.java
@@ -3,7 +3,9 @@ package it.polimi.db2.db2_project.web.utils;
 import java.util.Base64;
 
 public class ImageUtil {
-    public String getImgData(byte[] byteData) {
+
+    public static String getImgData(byte[] byteData) {
         return Base64.getMimeEncoder().encodeToString(byteData);
     }
+
 }
