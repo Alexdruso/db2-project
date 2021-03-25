6413da0850f190d1331b198ee01d06f0f94bde7f
Davide Merli <davide.merli@outlook.com>
2021-03-25 18:48:31 +0100

    feat: add admin-panel initial page

---
+++ src/main/java/it/polimi/db2/db2_project/web/controllers/AdminPanelController.java
@@ -0,0 +1,86 @@
+package it.polimi.db2.db2_project.web.controllers;
+
+import it.polimi.db2.db2_project.entities.QuestionnaireEntity;
+import it.polimi.db2.db2_project.entities.UserEntity;
+import it.polimi.db2.db2_project.services.AdminService;
+import it.polimi.db2.db2_project.services.ProductService;
+import it.polimi.db2.db2_project.web.TemplatingServlet;
+import org.thymeleaf.templatemode.TemplateMode;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
+@WebServlet(name = "AdminPanelController", value = "/AdminPanelController")
+public class AdminPanelController extends TemplatingServlet {
+
+    @EJB
+    private AdminService adminService;
+
+    @EJB
+    private ProductService productService;
+
+    private final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public AdminPanelController() {
+        super("admin-panel", TemplateMode.HTML, "WEB-INF/templates/", ".html");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HashMap<String, Object> context = new HashMap<>();
+
+        HttpSession session = request.getSession();
+        UserEntity user = (UserEntity) session.getAttribute("user");
+
+        if (session.isNew() || user == null) {
+            String path = getServletContext().getContextPath() + "/login";
+            response.sendRedirect(path);
+            return;
+        }
+
+        context.put("username", user.getUsername());
+        context.put("isAdmin", user.getAdmin());
+
+        super.processTemplate(request, response, context);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //check user logged in
+        HttpSession session = request.getSession();
+        UserEntity user = (UserEntity) session.getAttribute("user");
+
+        if (session.isNew() || user == null) {
+            String path = getServletContext().getContextPath() + "/";
+            response.sendRedirect(path);
+            return;
+        }
+
+        long userId = user.getId();
+        long productId = Long.parseLong(request.getParameter("product"));
+        Date questionnaireDate;
+
+        try {
+            questionnaireDate = dateFormat.parse(request.getParameter("date"));
+        } catch (ParseException e) {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, "Invalid date format");
+            return;
+        }
+
+        QuestionnaireEntity newQuestionnaire = adminService.createQuestionnaire(userId, productId, questionnaireDate);
+
+        String redirect = String.format("%s/edit-questionnaire?id=%d",
+                getServletContext().getContextPath(), newQuestionnaire.getId());
+
+        response.sendRedirect(redirect);
+    }
+}
