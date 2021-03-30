2389810302dc1778da13644da1910ff4bcaa7aa5
Dario Passarello <dario.passarello@gmail.com>
2021-03-30 23:12:30 +0200

    fix: minor fixes on site

--- src/main/java/it/polimi/db2/db2_project/web/controllers/RegistrationController.java
+++ src/main/java/it/polimi/db2/db2_project/web/controllers/RegistrationController.java
@@ -45,7 +45,7 @@ public class RegistrationController extends TemplatingServlet {
             return;
         }
 
-        Optional<UserEntity> result = userService.createUser(username, email, password);
+        Optional<UserEntity> result = userService.createUser(username, password, email);
 
         if (result.isEmpty()) {
             response.sendRedirect(getServletContext().getContextPath() + "/registration");
